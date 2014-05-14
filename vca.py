25813bf03011d84cc65bc0a04a1b20a3bb62fd0b
XgAdam <am969@york.ac.uk>
2014-05-14 13:14:35 +0100

    Create vca.py

---
+++ vca.py
@@ -0,0 +1,19 @@
+from visual import *
+import numpy as np
+import re
+Stage=1
+data=open('gulpin')
+condition_initial = False
+condition_final= False
+for line in data:
+    if Stage==1:
+        if 'cartesian region 1' in line:
+            condition_initial = True
+        if condition_initial == True:
+            if 'Ca ' in line:
+                posi=re.findall("\d+.\d+", line)
+                posi=[float(i) for i in posi]
+                posi=np.array(posi[:3])
+                sphere(pos=posi, radius=0.25, color=color.yellow)
+        if 'Constraints' in line:
+            condition_initial = False
